9390c2b59324b7c657b5c0d82c73d81cab6a29ec
Nephtys <christopher.kaag@gmail.com>
2016-12-26 22:20:12 +0100

    read from file used

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,19 @@ use iron::prelude::*;
 use router::Router;
 use std::ops::Deref;
 use rustc_serialize::json;
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
     print!("Starting server... ");
 
-    let json = r##"{"redirect_endpoint":"redirectendpoint","login_endpoint":"login","use_ssl":false,"sslcertpath":"./cert.p12","certpassword":"password","client_id":"myclientid","project_id":"myprojectid","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v1/certs","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"mysecret","redirect_url":"redirect_url","raw_host":"localhost","port":9123}"##;
+    //let json = r##"{"redirect_endpoint":"redirectendpoint","login_endpoint":"login","use_ssl":false,"sslcertpath":"./cert.p12","certpassword":"password","client_id":"myclientid","project_id":"myprojectid","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v1/certs","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"mysecret","redirect_url":"redirect_url","raw_host":"localhost","port":9123}"##;
+    let mut f = (File::open("config.json")).unwrap();
+    let mut buffer = String::new();
+    let size = f.read_to_string(&mut buffer);
+    println!("Have read in {0} bytes from file! As String: {1}", size.unwrap(), buffer);
 
-    let config : oidc::ConfigStruct = json::decode(&json).unwrap();
+    let config : oidc::ConfigStruct = json::decode(&buffer).unwrap();
 
     let config2 = config.clone();
     let local_config = config.clone();
