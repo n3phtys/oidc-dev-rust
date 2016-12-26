ad9a818b11022189d335412a4b0f9da42f01dc33
Nephtys <christopher.kaag@gmail.com>
2016-12-26 20:55:55 +0100

    uncompile part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+mod oidc;
+
+extern crate iron;
+extern crate router;
+
+use std::net::{TcpStream,ToSocketAddrs, SocketAddr};
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Starting server... ");
+
+    let config : oidc::ConfigStruct = oidc::ConfigStruct {
+        redirect_endpoint : "redirectendpoint".to_owned(),
+        login_endpoint : "login".to_owned(),
+        use_ssl: false,
+        sslcertpath : "./cert.p12".to_owned(),
+        certpassword : "password".to_owned(),
+        client_id : "myclientid".to_owned(),
+        project_id : "myprojectid".to_owned(),
+        auth_uri : "https://accounts.google.com/o/oauth2/auth".to_owned(),
+        token_uri : "https://www.googleapis.com/oauth2/v1/certs".to_owned(),
+        auth_provider_x509_cert_url : "https://www.googleapis.com/oauth2/v1/certs".to_owned(),
+        client_secret : "mysecret".to_owned(),
+        redirect_url : "redirect_url".to_owned(),
+        raw_host : "localhost".to_owned(),
+        port : 9123,
+    };
+
+    let redirect_closure = move |req : &mut Request| oidc::redirection_handler(oidc::ConfigStruct {
+        redirect_endpoint : "redirectendpoint".to_owned(),
+        login_endpoint : "login".to_owned(),
+        use_ssl: false,
+        sslcertpath : "./cert.p12".to_owned(),
+        certpassword : "password".to_owned(),
+        client_id : "myclientid".to_owned(),
+        project_id : "myprojectid".to_owned(),
+        auth_uri : "https://accounts.google.com/o/oauth2/auth".to_owned(),
+        token_uri : "https://www.googleapis.com/oauth2/v1/certs".to_owned(),
+        auth_provider_x509_cert_url : "https://www.googleapis.com/oauth2/v1/certs".to_owned(),
+        client_secret : "mysecret".to_owned(),
+        redirect_url : "redirect_url".to_owned(),
+        raw_host : "localhost".to_owned(),
+        port : 9123,
+    }, req);
+    //let login_closure = move |req : &mut Request| oidc::login_handler(config, req);
+
+
+    let mut router = Router::new();
+    router.get(format!("{}{}", "/".to_owned(), config.login_endpoint), redirect_closure, "login");
+    //router.get(format!("{}{}", "/".to_owned(), config.redirect_endpoint), oidc::redirection_handler(&config), "redirect");
+
+    let protocol = if config.use_ssl {
+        "https://"
+    } else {
+        "http://"
+    };
+
+    let address : std::net::SocketAddr = (format!("{}{}{}", protocol, config.raw_host, config.port)).to_socket_addrs().unwrap().nth(0).unwrap();
+
+    Iron::new(router).http(address).unwrap();
+
+    println!("started!")
 }
--- src/oidc.rs
+++ src/oidc.rs
@@ -1,16 +1,87 @@
 extern crate rustc_serialize;
-use rustc_serialize::json;
+extern crate iron;
+extern crate params;
+extern crate std;
 
+use self::rustc_serialize::base64::FromBase64;
+use self::iron::prelude::*;
+use self::iron::modifiers::Redirect;
+use self::iron::{Url, status};
+use self::params::{Params, Value};
 
-fn redirection_handler() {
+#[derive(Clone)]
+pub struct ConfigStruct  {
+    pub redirect_endpoint: String,
+    pub login_endpoint : String,
+    pub use_ssl: bool,
+    pub sslcertpath : String,
+    pub certpassword : String,
+    pub client_id : String,
+    pub project_id : String,
+    pub auth_uri : String,
+    pub token_uri : String,
+    pub auth_provider_x509_cert_url : String,
+    pub client_secret : String,
+    pub redirect_url : String,
+    pub raw_host : String,
+    pub port : u16,
+}
+
+#[derive(Clone)]
+pub struct RemoteVerifiedTokenStruct  {
+    iss: String,
+    iat: u64,
+    exp: u64,
+    at_hash: String,
+    aud: String,
+    sub: String,
+    email_verified: String,
+    azp: String,
+    email: String,
+    alg: String,
+    kid: String,
+}
+
+pub fn redirection_handler(config : ConfigStruct, req : &mut Request) ->  IronResult<Response>   {
+
+        /*
+            TODO: extract token and redirect url from state, etc.
+            */
+        let token = String::from("abc");
+        let url : String = String::from("abc.com");
+        let bearer : String = format_token_as_bearer_token(&token);
+        let together : String = format!("{}{}{}", url, "?token=".to_owned() , bearer);
+        let url = Url::parse(&together).unwrap();
+        Ok(Response::with((status::Found, Redirect(url.clone()))))
 
 }
 
-fn login_handler() {
-
+pub fn login_handler(config : ConfigStruct, req : &mut Request) -> IronResult<Response> {
+    let map : &params::Map = req.get_ref::<Params>().unwrap();
+    println!("Parameter Map: {:?}", map);
+    match map.get("url") {
+        Some(&Value::String(ref url64))  => {
+            let para = url64.from_base64().unwrap();
+            let decode = std::str::from_utf8(&para).unwrap();
+            let url = Url::parse("http://rust-lang.org").unwrap(); //TODO: implement this
+            Ok(Response::with((status::Found, Redirect(url.clone()))))
+        },
+        _ => Ok(Response::with(iron::status::NotFound)),
+    }
 }
 
-fn verify_handler() {
-
+fn exchange_code_for_access_token(config : &ConfigStruct, code : &String) -> String {
+        //TODO: make HTTP post call and wait for return
+    "this is returned".to_owned()
 }
 
+fn format_token_as_bearer_token(token : &String) -> String {
+    //TODO: url encoded and so on
+    "this is a token".to_owned()
+}
+
+
+/* //TODO:
+ fn verify_token() {
+
+}*/
